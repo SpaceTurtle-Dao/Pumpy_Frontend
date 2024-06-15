b945ed17f2b5e6e3336d72c4ba8861c298f8f24a
Jonathan Green <dev.jonathan.green@gmail.com>
2024-06-14 23:56:07 -0400

    WIP

--- src/lib/components/pumpForm.svelte
+++ src/lib/components/pumpForm.svelte
@@ -5,7 +5,7 @@
 		name: z.string().min(2).max(50),
 		ticker: z.string().min(2).max(50),
 		description: z.string().min(100).max(500),
-		image: z.string().url(),
+		image: z.string(),
 		twitter: z.string().url(),
 		telegram: z.string().url(),
 		website: z.string().url()
@@ -34,34 +34,69 @@
 		}
 	});
 
-	const { form: formData, enhance } = form;
+	const { form: formData, errors, enhance } = form;
 </script>
 
-<form action="/?/pumpForm" method="POST" class="w-2/3 space-y-6" use:enhance>
-	<Form.Field {form} name="name">
-		<Form.Control let:attrs>
-			<Form.Label>name</Form.Label>
-			<Input {...attrs} bind:value={$formData.name} />
-			<Form.Label>ticker</Form.Label>
-			<Input {...attrs} bind:value={$formData.ticker} />
-			<Form.Label>description</Form.Label>
-			<Textarea {...attrs} bind:value={$formData.description} />
-			<div class="grid w-full max-w-sm items-center gap-1.5">
-				<Form.Label for="picture">Picture</Form.Label>
-				<Input id="picture" type="file" />
-			</div>
-			<Form.Label>twitter</Form.Label>
-			<Input {...attrs} bind:value={$formData.twitter} />
-			<Form.Label>telegram</Form.Label>
-			<Input {...attrs} bind:value={$formData.telegram} />
-			<Form.Label>website</Form.Label>
-			<Input {...attrs} bind:value={$formData.website} />
-		</Form.Control>
-		<Form.Description>Cost to deploy: ~0.02 ICP</Form.Description>
-		<Form.FieldErrors />
-	</Form.Field>
-	<Form.Button>Submit</Form.Button>
-	{#if browser}
-		<SuperDebug data={$formData} />
-	{/if}
-</form>
+<div class="w-full flex justify-center">
+	<form method="POST" class="space-y-6" use:enhance>
+		<Form.Field {form} name="name" class="space-y-3 w-96">
+			<Form.Control let:attrs>
+				<div>
+					<Form.Label>name</Form.Label>
+					<Input {...attrs} bind:value={$formData.name} />
+					{#if $errors.name}
+						<small>{$errors.name}</small>
+					{/if}
+				</div>
+				<div>
+					<Form.Label>ticker</Form.Label>
+					<Input {...attrs} bind:value={$formData.ticker} />
+					{#if $errors.ticker}
+						<small>{$errors.ticker}</small>
+					{/if}
+				</div>
+				<div>
+					<Form.Label>description</Form.Label>
+					<Textarea {...attrs} bind:value={$formData.description} />
+					{#if $errors.description}
+						<small>{$errors.description}</small>
+					{/if}
+				</div>
+				<div class="grid w-full max-w-sm items-center gap-1.5">
+					<Form.Label for="picture">Picture</Form.Label>
+					<Input id="picture" type="file" bind:value={$formData.image} />
+					{#if $errors.image}
+						<small>{$errors.image}</small>
+					{/if}
+				</div>
+				<div>
+					<Form.Label>twitter</Form.Label>
+					<Input {...attrs} bind:value={$formData.twitter} />
+					{#if $errors.twitter}
+						<small>{$errors.twitter}</small>
+					{/if}
+				</div>
+				<div>
+					<Form.Label>telegram</Form.Label>
+					<Input {...attrs} bind:value={$formData.telegram} />
+					{#if $errors.telegram}
+						<small>{$errors.telegram}</small>
+					{/if}
+				</div>
+				<div>
+					<Form.Label>website</Form.Label>
+					<Input {...attrs} bind:value={$formData.website} />
+					{#if $errors.website}
+						<small>{$errors.website}</small>
+					{/if}
+				</div>
+			</Form.Control>
+			<Form.Description>Cost to deploy: ~0.02 ICP</Form.Description>
+			<Form.FieldErrors />
+			<Form.Button class="w-full">Submit</Form.Button>
+			{#if browser}
+				<SuperDebug data={$formData} />
+			{/if}
+		</Form.Field>
+	</form>
+</div>
