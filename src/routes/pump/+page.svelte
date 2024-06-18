2c8dc9d27877941f79175c21d8a1cefb0d9fc5eb
Jonathan Green <dev.jonathan.green@gmail.com>
2024-06-18 10:54:10 -0400

    WIP

--- src/routes/pump/+page.svelte
+++ src/routes/pump/+page.svelte
@@ -1,103 +1,59 @@
-<script>
-	import { onMount } from 'svelte';
-	import { createChart } from 'lightweight-charts';
-
-	onMount(() => {
-		// @ts-ignore
-		function generateRandomCandlestickData(numCandles) {
-			// @ts-ignore
-			function getRandomPrice(basePrice, volatility) {
-				return basePrice + (Math.random() - 0.5) * volatility;
-			}
-
-			const data = [];
-			let basePrice = 100; // starting price
-			const volatility = 5; // max fluctuation of the price
-
-			for (let i = 0; i < numCandles; i++) {
-				const open = getRandomPrice(basePrice, volatility);
-				const close = getRandomPrice(open, volatility);
-				const high = Math.max(open, close) + Math.random() * volatility;
-				const low = Math.min(open, close) - Math.random() * volatility;
-				let time = new Date().getTime() + i * 60000;
-				let date = new Date(time);
-				var year = date.toLocaleString('default', { year: 'numeric' });
-				var month = date.toLocaleString('default', { month: '2-digit' });
-				var day = date.toLocaleString('default', { day: '2-digit' });
-				var hour = date.toLocaleString('default', { hour: '2-digit' });
-				// Generate yyyy-mm-dd date string
-				// @ts-ignore
-				// @ts-ignore
-				var formattedDate = year + '-' + month + '-' + day + '-' + hour;
-				data.push({
-					time: time, // assuming each candle represents 1 minute
-					open: Number(open.toFixed(2)),
-					high: Number(high.toFixed(2)),
-					low: Number(low.toFixed(2)),
-					close: Number(close.toFixed(2))
-				});
-
-				basePrice = close; // next candle's base price starts from the current close price
-			}
-
-			return data;
-		}
-
-		// Usage example:
-		const numCandles = 100;
-		// @ts-ignore
-		const candlestickData = generateRandomCandlestickData(numCandles);
-		console.log(candlestickData);
-
-		// @ts-ignore
-		// @ts-ignore
-		// @ts-ignore
-		// @ts-ignore
-		// @ts-ignore
-		const chartOptions = {
-			layout: { textColor: 'black', background: { type: 'solid', color: 'white' } }
-		};
-		//const chart = createChart(document.body, { width: 400, height: 300 });
-		// @ts-ignore
-
-		const chart = createChart('container', {
-			width: 1500,
-			height: 450,
-			layout: {
-				// @ts-ignore
-				background: { type: 'solid', color: 'white' },
-				textColor: 'rgba(33, 56, 77, 1)'
-			},
-			localization: {
-				locale: 'cn-CN'
-			},
-			grid: {
-				vertLines: {
-					color: 'rgba(197, 203, 206, 0.7)'
-				},
-				horzLines: {
-					color: 'rgba(197, 203, 206, 0.7)'
-				}
-			},
-			timeScale: {
-				timeVisible: true,
-				secondsVisible: false
-			}
-		});
-		const candlestickSeries = chart.addCandlestickSeries({
-			upColor: '#26a69a',
-			downColor: '#ef5350',
-			borderVisible: false,
-			wickUpColor: '#26a69a',
-			wickDownColor: '#ef5350'
-		});
-		// @ts-ignore
-		candlestickSeries.setData(candlestickData);
-
-		chart.timeScale().fitContent();
-		// @ts-ignore
-		chart.timeScale().scrollToPosition(5);
-	});
+<script lang="ts">
+	import Chart from '$lib/components/chart.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Progress } from '$lib/components/ui/progress/index.js';
+	import Trades from '$lib/components/trades.svelte';
 </script>
 
-<div id="container"></div>
+<div class="flex flex-row w-full gap-10">
+	<div class="w-full basis-2/3">
+		<div class="flex flex-col space-y-8">
+			<Chart />
+			<Trades/>
+		</div>
+	</div>
+	<div class="flex flex-col space-y-4">
+		<Card.Root class="w-[350px] space-y-6">
+			<Card.Header>
+				<div class="flex flex-row gap-2">
+					<Button class="w-full">Buy</Button>
+					<Button class="w-full" variant="secondary">Sell</Button>
+				</div>
+			</Card.Header>
+			<Card.Content>
+				<div class="flex flex-col space-y-4">
+					<div class="flex flex-row justify-between">
+						<Button class="h-6 ">switch to ICP</Button>
+						<Button class="h-6">set max slippage</Button>
+					</div>
+					<Input type="number" placeholder="Name of your project" />
+					<Button class="w-full">place trade</Button>
+				</div>
+			</Card.Content>
+		</Card.Root>
+		<div class="flex flex-row gap-4">
+			<Button class="w-full" variant="link">Twitter</Button>
+			<Button class="w-full" variant="link">Discord</Button>
+			<Button class="w-full" variant="link">Telegram</Button>
+		</div>
+		<Card.Root
+			data-x-chunk-name="dashboard-05-chunk-1"
+			data-x-chunk-description="A stats card showing this week's total sales in USD, the percentage difference from last week, and a progress bar."
+		>
+			<Card.Header class="pb-2">
+				<Card.Description>Liquidty</Card.Description>
+				<Card.Title class="text-4xl">$1329</Card.Title>
+			</Card.Header>
+			<Card.Content class="flex flex-col space-y-2">
+				<div class="text-xs text-muted-foreground">25% bonded</div>
+			</Card.Content>
+			<Card.Footer>
+				<Progress value={25} aria-label="25% increase" />
+			</Card.Footer>
+		</Card.Root>
+	</div>
+</div>
