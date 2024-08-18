87c95e8a1d20c1d1db806e6181d5adaa7280fb81
Jonathan Green <dev.jonathan.green@gmail.com>
2024-08-18 15:15:46 -0400

    WIP

---
+++ src/lib/utils/toastHandler.svelte
@@ -0,0 +1,54 @@
+<script context="module" lang="ts">
+	import { toast } from 'svelte-sonner';
+
+	enum StatusCode {
+		NotFound = 404,
+		Success = 200,
+		Accepted = 202,
+		BadRequest = 400,
+		None
+	}
+
+	export const createToast = (statusCode: StatusCode,title:string,description:string,label:string,messageId:string) => {
+		switch (statusCode) {
+			case StatusCode.NotFound: {
+				toast.error(title, {
+					description: description
+				});
+				break;
+			}
+			case StatusCode.Success: {
+				toast.success(title, {
+					description: description,
+					action: {
+						label: label,
+						onClick: () => window.open('https://www.ao.link/#/message/'+messageId, '_blank')
+					}
+				});
+				break;
+			}
+			case StatusCode.Accepted: {
+				toast.success(title, {
+					description: description,
+					action: {
+						label: label,
+						onClick: () => window.open('https://www.ao.link/#/message/'+messageId, '_blank')
+					}
+				});
+				break;
+			}
+			case StatusCode.BadRequest: {
+				toast.error(title, {
+					description: description
+				});
+				break;
+			}
+			case StatusCode.None: {
+				toast.info(title, {
+					description: description
+				});
+				break;
+			}
+		}
+	};
+</script>
